8256514e64aaeb5fd998b19394bd9c63e9668084
Giacomo Guidotto <giacomo.guidotto@gmail.com>
2025-10-07 17:23:01 +0200

    feat: bootstrap ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,98 +4,58 @@ export default function Home() {
   return (
     <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-sans sm:p-20">
       <main className="row-start-2 flex flex-col items-center gap-[32px] sm:items-start">
-        <Image
-          alt="Next.js logo"
-          className="dark:invert"
-          height={38}
-          priority
-          src="/next.svg"
-          width={180}
-        />
-        <ol className="list-inside list-decimal text-center font-mono text-sm/6 sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="rounded bg-black/[.05] px-1 py-0.5 font-mono font-semibold dark:bg-white/[.06]">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+        <h1 className="text-4xl font-bold">guidotto.dev</h1>
+        <p className="font-mono text-sm/6 sm:text-left">site under construction... hold to your horses</p>
 
         <div className="flex flex-col items-center gap-4 sm:flex-row">
           <a
             className="flex h-10 items-center justify-center gap-2 rounded-full border border-transparent border-solid bg-foreground px-4 font-medium text-background text-sm transition-colors hover:bg-[#383838] sm:h-12 sm:w-auto sm:px-5 sm:text-base dark:hover:bg-[#ccc]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            href="https://github.com/GiacomoGuidotto"
             rel="noopener noreferrer"
             target="_blank"
           >
             <Image
-              alt="Vercel logomark"
-              className="dark:invert"
+              alt="GitHub logo"
+              className="invert dark:invert-0"
               height={20}
-              src="/vercel.svg"
+              src="/github.svg"
               width={20}
             />
-            Deploy now
+            GitHub
           </a>
           <a
-            className="flex h-10 w-full items-center justify-center rounded-full border border-black/[.08] border-solid px-4 font-medium text-sm transition-colors hover:border-transparent hover:bg-[#f2f2f2] sm:h-12 sm:w-auto sm:px-5 sm:text-base md:w-[158px] dark:border-white/[.145] dark:hover:bg-[#1a1a1a]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+            className="flex h-10 w-full items-center justify-center gap-2 rounded-full border border-solid border-[#0A66C2] bg-[#0A66C2] px-4 font-medium text-sm text-white transition-colors hover:bg-[#004182] hover:border-[#004182] sm:h-12 sm:w-auto sm:px-5 sm:text-base md:w-[158px] dark:bg-[#0A66C2] dark:border-[#0A66C2] dark:hover:bg-[#004182] dark:hover:border-[#004182]"
+            href="https://www.linkedin.com/in/giacomo-guidotto/"
             rel="noopener noreferrer"
             target="_blank"
           >
-            Read our docs
+            <Image
+              alt="LinkedIn logo"
+              className=""
+              height={20}
+              src="/linkedin.webp"
+              width={20}
+            />
+            LinkedIn
           </a>
         </div>
       </main>
       <footer className="row-start-3 flex flex-wrap items-center justify-center gap-[24px]">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          <Image
-            alt="File icon"
-            aria-hidden
-            height={16}
-            src="/file.svg"
-            width={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          <Image
-            alt="Window icon"
-            aria-hidden
-            height={16}
-            src="/window.svg"
-            width={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
           rel="noopener noreferrer"
           target="_blank"
         >
+          Powered by
           <Image
-            alt="Globe icon"
+            alt="Next.js logo"
+            className="ml-2 dark:invert"
             aria-hidden
-            height={16}
-            src="/globe.svg"
-            width={16}
+            height={28}
+            src="/next.svg"
+            width={120}
           />
-          Go to nextjs.org →
         </a>
       </footer>
     </div>
