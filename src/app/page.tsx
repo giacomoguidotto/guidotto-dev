9a89436b00ebbb777c398ca4184cf4b6298aebe2
Giacomo Guidotto <giacomo.guidotto@gmail.com>
2025-10-07 17:30:06 +0200

    chore: increase horsing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-sans sm:p-20">
       <main className="row-start-2 flex flex-col items-center gap-[32px] sm:items-start">
         <h1 className="text-4xl font-bold">guidotto.dev</h1>
-        <p className="font-mono text-sm/6 sm:text-left">site under construction... hold to your horses</p>
+        <p className="font-mono text-sm/6 sm:text-left">site under construction... hold to your horses 🐴</p>
 
         <div className="flex flex-col items-center gap-4 sm:flex-row">
           <a
